f9036d895a200d68354f440e8d8e31bb63b0922c
NamHoJin20 <163081950+NamHoJin20@users.noreply.github.com>
2024-04-16 13:59:18 +0900

    append text

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,12 @@ import TodoList from "@/components/TodoList";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">Todo List</h1>
+      <h1 className="text-3xl font-bold mb-4">Hello world!</h1>
+      <h2 className="text-3xl font-bold mb-4">My name is hojin</h2>
       <TodoList />
     </div>
   );
 }
+
